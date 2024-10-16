ed3fd566669aac202f2e82ba79e648203deec281
Tanishq Uppal <tanishquppal220@gmail.com>
2024-10-16 18:52:24 +0000

    Datatypes Done

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"    // Enable strict mode 
+/* This means strictly use new js */
+
+/* it is used to give popup in browser */
+// it give error in node.js
+// alert("Hello World");
+
+// datatypes
+let name="Tanishq";
+let marks=90.4;
+let age=20;
+let isApproved=true;
+let firstName=undefined;
+let lastName=null;
+
+/* 
+number => 2^53
+string => " "
+boolean => true/false
+undefined => variable is declared but not assigned
+null => variable is empty
+ */
+
+// how to check type of variable
+console.table([typeof name,typeof marks,typeof age,typeof isApproved,typeof firstName,typeof lastName]);
